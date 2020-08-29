3e74afdf5fa144ad7e901d692f6680d5dac46e42
ktmweld87@gmail.com <ktmweld87@gmail.com>
2020-08-29 15:22:28 -0600

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,12 +71,15 @@ $("#trash").droppable({
   drop: function (event, ui) {
     ui.draggable.remove();
     console.log("drop");
+    $("bottom-trash").removeClass("bottom-trash-active");
   },
   over: function (event, ui) {
     console.log("over");
+    $("bottom-trash").addClass("bottom-trash-active");
   },
   out: function (event, ui) {
     console.log("out");
+    $("bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -94,7 +97,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -239,16 +242,25 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event) {
-    console.log("activate", this);
+
+    $(this).addClass("dropover");
+    $("bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function (event) {
-    console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $("bottom-trash").removeClass("bottom-trash-drag");
+    
+    
   },
   over: function (event) {
     console.log("over", event.target);
+    
+  
+    
   },
   out: function (event) {
     console.log("out", event.target);
+    
   },
   update: function (event) {
     // array to store the task data in
@@ -285,3 +297,8 @@ $("#modalDueDate").datepicker({
   minDate: 1
 });
 
+setInterval(function () {
+  $(".card .list-group-item").each(function (el) {
+    auditTask(el);
+  });
+}, 5000);
